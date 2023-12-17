1ec598e0fd3b0e1dd5de87442f8bb4696fbaf615
ABHISHEK <iabhishekmp@gmail.com>
2023-12-17 23:01:03 +0530

    Improvement

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -19,9 +19,17 @@ class _MainAppState extends State<MainApp> {
   late final ClientChannel _channel;
   late final SpeedTestClient _stub;
   ResponseStream<DownloadData>? stream;
+
+  late final TextEditingController _totalSizeController;
+  late final TextEditingController _chunkSizeController;
+
+  final stopwatch = Stopwatch();
   @override
   void initState() {
     super.initState();
+    _totalSizeController = TextEditingController(text: '100');
+    _chunkSizeController = TextEditingController(text: '1');
+
     _channel = ClientChannel(
       '141.148.203.16',
       port: int.parse(Platform.environment['port'] ?? '4001'),
@@ -36,17 +44,24 @@ class _MainAppState extends State<MainApp> {
   void startSpeedTest() async {
     final yo = await _stub.ping(Ping(text: "Pinging"));
     log(yo.value);
-    setState(() {
-      stream = null;
-    });
+    stopwatch.reset();
+    stopwatch.start();
     // 1,000,000 = 1 MB
     stream = _stub.downloadFile(
       DownloadRequest(
-        totalSize: 50000000,
-        chunkSize: 1000000,
+        totalSize: int.parse(_totalSizeController.text),
+        chunkSize: int.parse(_chunkSizeController.text),
       ),
     );
     setState(() {});
+  }
+
+  @override
+  void dispose() {
+    _totalSizeController.dispose();
+    _chunkSizeController.dispose();
+    _channel.shutdown();
+    super.dispose();
   }
 
   @override
@@ -58,32 +73,72 @@ class _MainAppState extends State<MainApp> {
           onPressed: startSpeedTest,
         ),
         body: Center(
-          child: stream == null
-              ? const Text('Please start')
-              : StreamBuilder<DownloadData>(
-                  stream: stream,
-                  builder: (context, snapshot) {
-                    if (snapshot.hasError) {
-                      log(
-                        "Error in stream",
-                        error: snapshot.error,
-                        stackTrace: snapshot.stackTrace,
-                      );
-                      return Text(snapshot.error.toString());
-                    }
-                    return Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Text('Connection State: ${snapshot.connectionState}'),
-                        Text('Progress: ${snapshot.data?.progress}'),
-                        if (snapshot.data != null)
-                          LinearProgressIndicator(
-                            value: (snapshot.data!.progress) / 100,
-                          )
-                      ],
-                    );
-                  },
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Padding(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 30,
+                  vertical: 10,
                 ),
+                child: Form(
+                  child: Column(
+                    children: [
+                      TextFormField(
+                        controller: _chunkSizeController,
+                        keyboardType: TextInputType.number,
+                        decoration: const InputDecoration(
+                          label: Text('ChunkSize'),
+                        ),
+                      ),
+                      TextFormField(
+                        controller: _totalSizeController,
+                        keyboardType: TextInputType.number,
+                        decoration: const InputDecoration(
+                          label: Text('TotalSize'),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              const SizedBox(height: 50),
+              stream == null
+                  ? const Text('Please start')
+                  : StreamBuilder<DownloadData>(
+                      stream: stream,
+                      builder: (context, snapshot) {
+                        if (snapshot.hasError) {
+                          log(
+                            "Error in stream",
+                            error: snapshot.error,
+                            stackTrace: snapshot.stackTrace,
+                          );
+                          return Text(snapshot.error.toString());
+                        }
+                        final progress = snapshot.data?.progress;
+                        if (snapshot.connectionState.name == "done") {
+                          stopwatch.stop();
+                        }
+                        return Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text(
+                                'Connection State: ${snapshot.connectionState}'),
+                            Text('Progress: $progress'),
+                            if (progress != null)
+                              LinearProgressIndicator(
+                                value: progress / 100,
+                              ),
+                            const SizedBox(height: 20),
+                            Text(
+                                'Time taken: ${stopwatch.elapsed.inMilliseconds / 1000} sec')
+                          ],
+                        );
+                      },
+                    ),
+            ],
+          ),
         ),
       ),
     );
